9760c612db00a3c38916d80ef2a3e3dc97c5e159
victor.chen <spartacus429496@gmail.com>
2015-06-05 18:21:58 +0800

    add execute remote commands and remote open tunnel #8

--- callbacks/cmd.py
+++ callbacks/cmd.py
@@ -1,6 +1,7 @@
 from callback import Callback
 import commands
 import logging
+import os
 
 class Cmd(Callback):
     def __init__(self):
@@ -23,7 +24,6 @@ class Cmd(Callback):
 
     def ssh_bind(self, args = None):
         """
-        ssh -R <local port>:<remote host>:<remote port> <SSH hostname>
         ssh -R sourcePort:forwardToHost:onPort connectToHost
         """
         self.log.debug('args:%s'%args.items(), extra={'namespace' : 'xmpp'})
@@ -31,13 +31,25 @@ class Cmd(Callback):
         source_port = args.get('source_port', '22')
         on_port = args.get('on_port', '9090')
         connect_to_host = args.get('connect_to_host', 'ibox@www.pinet.cc')
-        cmd = 'ssh -R %s:localhost:%s %s'%(source_port,on_port,connect_to_host) 
+        cmd = 'ssh -R %s:localhost:%s %s -Nf'%(on_port,source_port,connect_to_host) 
+        #cmd = 'ssh -R -Nf 9091:localhost:22 spirit@192.168.20.138'
         self.log.debug('cmd:%s  '%cmd, extra={'namespace' : 'xmpp'})
-        (status, output) = commands.getstatusoutput(cmd)
-        self.log.debug('[status]:%s  [output]:%s'%(status,output), extra={'namespace' : 'xmpp'})
-        if status != 0:
-            result = 'cmd execute error!'
-        else :
-                result = output
+
+        try:
+            pid = os.fork()
+            if pid == 0: 
+                self.log.debug('this is child process', extra={'namespace' : 'xmpp'})
+                (status, output) = commands.getstatusoutput(cmd)
+                self.log.debug('[status]:%s  [output]:%s'%(status,output), extra={'namespace' : 'xmpp'})
+                if status != 0:
+                    result = 'cmd execute error!'
+                else :
+                    result = 'OK'+output
+
+            else: 
+                result = 'OK parent'
+                self.log.debug('this is parent process', extra={'namespace' : 'xmpp'})
+        except:
+            result = 'fork error!'
         return result 
 
