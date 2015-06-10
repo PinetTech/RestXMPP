eb9f923c2e3e713e37380da5d0d323f7114eafec
victor.chen <spartacus429496@gmail.com>
2015-06-10 15:29:46 +0800

    fix bug: SSH -R process cannot exit #8

--- callbacks/cmd.py
+++ callbacks/cmd.py
@@ -6,12 +6,11 @@ import os
 class Cmd(Callback):
     def __init__(self):
         self.log = logging.getLogger("cement:app:xmpp")
+        self.log.debug('Cmd class creat object !', extra={'namespace' : 'xmpp'})
 
     def run(self, args = None):
         self.log.debug('args:%s'%args.items(), extra={'namespace' : 'xmpp'})
-        
         cmd = args.get('cmd', 'not found')
-
         self.log.debug('cmd:%s  '%cmd, extra={'namespace' : 'xmpp'})
         if cmd != 'not found':
             (status, output) = commands.getstatusoutput(cmd)
@@ -31,25 +30,9 @@ class Cmd(Callback):
         source_port = args.get('source_port', '22')
         on_port = args.get('on_port', '9090')
         connect_to_host = args.get('connect_to_host', 'ibox@www.pinet.cc')
-        cmd = 'ssh -R %s:localhost:%s %s -Nf'%(on_port,source_port,connect_to_host) 
-        #cmd = 'ssh -R -Nf 9091:localhost:22 spirit@192.168.20.138'
+        path = '/usr/local/src/RestXMPP/bin/'
+        cmd = 'cd %s ; sh ssh_xmpp %s %s %s '%(path,on_port,source_port,connect_to_host) 
         self.log.debug('cmd:%s  '%cmd, extra={'namespace' : 'xmpp'})
-
-        try:
-            pid = os.fork()
-            if pid == 0: 
-                self.log.debug('this is child process', extra={'namespace' : 'xmpp'})
-                (status, output) = commands.getstatusoutput(cmd)
-                self.log.debug('[status]:%s  [output]:%s'%(status,output), extra={'namespace' : 'xmpp'})
-                if status != 0:
-                    result = 'cmd execute error!'
-                else :
-                    result = 'OK'+output
-
-            else: 
-                result = 'OK parent'
-                self.log.debug('this is parent process', extra={'namespace' : 'xmpp'})
-        except:
-            result = 'fork error!'
+        os.system(cmd)
+        result = 'OK SSH Tunnel'
         return result 
-
